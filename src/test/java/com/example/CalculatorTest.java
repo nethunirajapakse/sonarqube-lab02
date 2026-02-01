9495d182d46c1723a17caaf79280be7e6b8cc796
Nethuni <nethunirajapakse@gmail.com>
2026-02-01 23:08:31 +0530

    Revert "Add more test coverage"
    This reverts commit c92b764f4c44deaf7fbdd52a5a0d180bda5e3487.

--- src/test/java/com/example/CalculatorTest.java
+++ src/test/java/com/example/CalculatorTest.java
@@ -5,45 +5,10 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class CalculatorTest {
 
-    Calculator calculator = new Calculator();
-
     @Test
     void addWorks() {
-        assertEquals(5, calculator.calculate(2, 3, Calculator.Operation.ADD));
-    }
-
-    @Test
-    void subWorks() {
-        assertEquals(1, calculator.calculate(4, 3, Calculator.Operation.SUB));
-    }
-
-    @Test
-    void mulWorks() {
-        assertEquals(6, calculator.calculate(2, 3, Calculator.Operation.MUL));
-    }
-
-    @Test
-    void divWorks() {
-        assertEquals(2, calculator.calculate(6, 3, Calculator.Operation.DIV));
-    }
-
-    @Test
-    void divByZeroReturnsZero() {
-        assertEquals(0, calculator.calculate(6, 0, Calculator.Operation.DIV));
-    }
-
-    @Test
-    void modWorks() {
-        assertEquals(1, calculator.calculate(7, 3, Calculator.Operation.MOD));
-    }
-
-    @Test
-    void powWorks() {
-        assertEquals(8, calculator.calculate(2, 3, Calculator.Operation.POW));
-    }
-
-    @Test
-    void defaultCaseReturnsZero() {
-        assertEquals(0, calculator.calculate(1, 1, null));
+        Calculator c = new Calculator();
+        assertEquals(5,
+                c.calculate(2, 3, Calculator.Operation.ADD));
     }
 }
